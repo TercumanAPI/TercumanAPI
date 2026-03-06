b15cee056e196f0b47ac1f685c63b2019c1191d4
MUSAB-KASAP <musaab19971999@gmail.com>
2026-03-06 23:22:30 +0300

    gender

--- Tercuman.Application/DTOs/Auth/RegisterDto.cs
+++ Tercuman.Application/DTOs/Auth/RegisterDto.cs
@@ -9,6 +9,7 @@ namespace Tercuman.Application.DTOs.Auth
 {
     public class RegisterDto
     {
+        //  FullName is required for registration, but we will also set the BaseEntity.Name to the same value in the AuthService.
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
--- Tercuman.Domin/Entities/Conversation.cs
+++ Tercuman.Domin/Entities/Conversation.cs
@@ -10,9 +10,7 @@ namespace Tercuman.Domain.Entities;
 public class Conversation
 {
     public Guid Id { get; set; }
-
     public Guid User1Id { get; set; }
     public Guid User2Id { get; set; }
-
     public List<Message> Messages { get; set; } = new();
 }
