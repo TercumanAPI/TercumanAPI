bedfc5626799db4aecbf9681a624db90adc8ed0d
MUSAB-KASAP <musaab19971999@gmail.com>
2026-03-09 22:02:31 +0300

    ilan detayında experienceLevel ve serviceType alanları eklendi

--- Tercuman.Application/DTOs/Listing/ListingDetailDto.cs
+++ Tercuman.Application/DTOs/Listing/ListingDetailDto.cs
@@ -1,32 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tercuman.Domin.Enums;
 
 namespace Tercuman.Application.DTOs.Listing
 {
     public class ListingDetailDto
-    {        
-            public Guid Id { get; set; }
-            public string Title { get; set; }
-            public string Description { get; set; }
-            public decimal Price { get; set; }
-            public string City { get; set; }
+    {
+        public Guid Id { get; set; }
 
-            // 🔥 User bilgileri
-            public string UserFullName { get; set; }
-            public string Gender { get; set; }
-            public DateTime UserCreatedAt { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
 
-            // 🔥 Listing bilgileri
-            public int ViewCount { get; set; }
-            public DateTime CreatedAt { get; set; }
-            //public DateTime CreatedDate { get; set; }
-            public List<string> Images { get; set; }
-            public ExperienceLevel ExperienceLevel { get; set; }
-            public ServiceType ServiceType { get; set; }
-        
+        public string City { get; set; } = "";
+
+        // 🔥 User bilgileri
+        public string UserFullName { get; set; } = "";
+        public string Gender { get; set; } = "";
+        public DateTime UserCreatedAt { get; set; }
+
+        // 🔥 Listing bilgileri
+        public int ViewCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<string> Images { get; set; } = new();
+
+        public ExperienceLevel ExperienceLevel { get; set; }
+        public ServiceType ServiceType { get; set; }
     }
 }
