8ef5d972b31b97c8c587a1da2552da29b0613cfd
MUSAB-KASAP <musaab19971999@gmail.com>
2026-03-25 16:09:33 +0300

    Fix MAUI app and shell initialization for XAML loading

--- Tercuman.Mobile/App.xaml.cs
+++ Tercuman.Mobile/App.xaml.cs
@@ -4,19 +4,9 @@ namespace Tercuman.Mobile;
 
 public partial class App : Application
 {
-    public static IServiceProvider Services { get; private set; } = default!;
-
-    public App(IServiceProvider serviceProvider)
+    public App()
     {
-        // --- BURAYA YAPIŞTIRIN ---
-        var names = typeof(App).Assembly.GetManifestResourceNames();
-        System.Diagnostics.Debug.WriteLine("Manifest resources:\n" + string.Join("\n", names));
-        // -------------------------
         InitializeComponent();
-
-        Services = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
-
-        //  DI ile AppShell oluştur
-        MainPage = serviceProvider.GetRequiredService<AppShell>();
+        MainPage = new AppShell();
     }
 }
--- Tercuman.Mobile/AppShell.xaml.cs
+++ Tercuman.Mobile/AppShell.xaml.cs
@@ -1,34 +1,15 @@
-﻿using Tercuman.Mobile.Services;
-using Tercuman.Mobile.View;
+﻿using Tercuman.Mobile.View;
 
 namespace Tercuman.Mobile;
 
 public partial class AppShell : Shell
 {
-    private readonly AuthService _authService;
-
-    public AppShell(AuthService authService)
+    public AppShell()
     {
         InitializeComponent();
 
-        _authService = authService;
-
-        // 🔥 ROUTES
         Routing.RegisterRoute("login", typeof(LoginPage));
         Routing.RegisterRoute("listings", typeof(ListingsPage));
         Routing.RegisterRoute("detail", typeof(ListingDetailPage));
-
-        // 🔥 async fire & forget
-        _ = InitializeApp();
-    }
-
-    private async Task InitializeApp()
-    {
-        var isLoggedIn = await _authService.IsLoggedInAsync();
-
-        if (isLoggedIn)
-            await GoToAsync("//listings");
-        else
-            await GoToAsync("//login");
     }
 }
