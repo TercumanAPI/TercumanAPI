65444672f1da8b7e30a7965d03d394e7bcac64fe
MUSAB-KASAP <musaab19971999@gmail.com>
2026-03-25 16:04:50 +0300

    maui hata3

--- Tercuman.Mobile/App.xaml.cs
+++ Tercuman.Mobile/App.xaml.cs
@@ -8,11 +8,15 @@ public partial class App : Application
 
     public App(IServiceProvider serviceProvider)
     {
+        // --- BURAYA YAPIŞTIRIN ---
+        var names = typeof(App).Assembly.GetManifestResourceNames();
+        System.Diagnostics.Debug.WriteLine("Manifest resources:\n" + string.Join("\n", names));
+        // -------------------------
         InitializeComponent();
 
-        Services = serviceProvider;
+        Services = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
 
-        // 🔥 DI ile AppShell oluştur
+        //  DI ile AppShell oluştur
         MainPage = serviceProvider.GetRequiredService<AppShell>();
     }
 }
