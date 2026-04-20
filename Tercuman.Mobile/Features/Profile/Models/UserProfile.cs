723d96ca753cbbee7245201aa4f06ac6a892d22b
ferit.ekta <renault.laguna.973@gmail.com>
2026-04-20 13:51:55 +0300

    admin hatasıçözüldü

---
+++ Tercuman.Mobile/Features/Profile/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tercuman.Mobile.Features.Profile.Models;
+
+public class UserProfile
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Gender { get; set; }
+    public string Bio { get; set; } // Hakkımda kısmı için
+}
